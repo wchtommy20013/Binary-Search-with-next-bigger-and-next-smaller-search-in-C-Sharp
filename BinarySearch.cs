06aa585e1f4a96d4cca04233b7fe6edd20550943
wchtommy20013 <tommystudy20013@gmail.com>
2017-02-16 03:56:06 +0800

    v1.0 Release

---
+++ BinarySearch.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearch {
+    class BinarySearch {
+        /**
+         * This method is used to find exact position of num 
+         * in arr with binary search method.
+         * @param num       The number to be found.
+         * @param arr       A pre-sorted non-repeating ascending array to be searched.
+         * @param arrlen    This is the size of arr.
+         * @return int      This returns position of num in arr. (-1 for not found)
+         */
+        public static int Exact(int num, int[] arr, int arrlen) {
+            int high = arrlen - 1;
+            int low = 0;
+            while (low <= high) {
+                int mid = (high + low) / 2;
+                if (num == arr[mid]) return mid;
+                else if (arr[mid] < num) low = mid + 1;
+                else if (arr[mid] > num) high = mid - 1;
+            }
+            return -1;
+        }
+
+        /**
+         * This method is used to find position of next bigger integer 
+         * compare to num in arr with binary search method.
+         * @param num       The number to be found.
+         * @param arr       A pre-sorted non-repeating ascending array to be searched.
+         * @param arrlen    This is the size of arr.
+         * @return int      This returns position of num in arr. (-1 for not found)
+         */
+        public static int NextBigger(int num, int[] arr, int arrlen) {
+            int high = arrlen - 1;
+            int low = 0;
+            while (low <= high) {
+                int mid = (high + low) / 2;
+                if (num == arr[mid]) return mid + 1;
+                else if (arr[mid] < num) low = mid + 1;
+                else if (arr[mid] > num) high = mid - 1;
+            }
+
+            if (high < 0) return 0;
+            else if (low > arrlen - 1) return -1;
+            else return low;
+        }
+
+        /**
+         * This method is used to find position of next smaller integer 
+         * compare to num in arr with binary search method.
+         * @param num       The number to be found.
+         * @param arr       A pre-sorted non-repeating ascending array to be searched.
+         * @param arrlen    This is the size of arr.
+         * @return int      This returns position of num in arr. (-1 for not found)
+         */
+        public static int NextSmaller(int num, int[] arr, int arrlen) {
+            int high = arrlen - 1;
+            int low = 0;
+            while (low <= high) {
+                int mid = (high + low) / 2;
+                if (num == arr[mid]) return mid - 1;
+                else if (arr[mid] < num) low = mid + 1;
+                else if (arr[mid] > num) high = mid - 1;
+            }
+
+            if (high < 0) return -1;
+            else if (low > arrlen - 1) return arrlen - 1;
+            else return high;
+        }
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BinarySearch {
+    class Test {
+        static void Main(string[] args) {
+            int[] arr = { 1, 2, 3, 4, 6, 7, 8, 11, 12, 13, 13, 13, 14};
+            int value = 13;
+            int exact = BinarySearch.Exact(value, arr, arr.Length);
+            int bigger = BinarySearch.NextBigger(value, arr, arr.Length);
+            int smaller = BinarySearch.NextSmaller(value, arr, arr.Length);
+            Console.Write("{");
+            for(int i = 0; i < arr.Length - 1; i++) {
+                Console.Write(" " + arr[i] + ",");
+            }
+            Console.WriteLine( " " + arr[arr.Length - 1] + " }");
+            Console.WriteLine("Finding: " + value);
+            Console.WriteLine("------------------------");
+            Console.WriteLine("Exact position: " + (exact == -1 ? "Not found" : exact.ToString()));
+            Console.WriteLine("Next Bigger Value: " + (bigger == -1 ? "Not found" : arr[bigger].ToString()));
+            Console.WriteLine("Next Smaller Value: " + (smaller == -1 ? "Not found" : arr[smaller].ToString()));
+
+            Console.ReadKey();
+        }
+    }
+}
